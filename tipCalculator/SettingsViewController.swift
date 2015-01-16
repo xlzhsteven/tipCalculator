e311570ff3e48de5415c5462331a81489711c429
Xiaolong Zhang <xlzhsteven@gmail.com>
2015-01-15 23:54:36 -0800

    Initial Commit

---
+++ tipCalculator/SettingsViewController.swift
@@ -0,0 +1,97 @@
+//
+//  SettingsViewController.swift
+//  tipCalculator
+//
+//  Created by Xiaolong Zhang on 1/14/15.
+//  Copyright (c) 2015 Xiaolong Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipOne: UITextField!
+    @IBOutlet weak var tipTwo: UITextField!
+    @IBOutlet weak var tipThree: UITextField!
+
+    @IBOutlet weak var sliderOne: UISlider!
+    @IBOutlet weak var sliderTwo: UISlider!
+    @IBOutlet weak var sliderThree: UISlider!
+    
+    @IBOutlet weak var selectedTheme: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "Define Tip %"
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func opTap(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func firstTipSlider(sender: AnyObject) {
+        var currentValue = sliderOne.value
+        tipOne.text = String(format: "%.2f", currentValue)
+        setTipValue(tipOne.text, tipIndex: 0)
+    }
+
+    @IBAction func secondTipSlider(sender: AnyObject) {
+        var currentValue = sliderTwo.value
+        tipTwo.text = String(format: "%.2f", currentValue)
+        setTipValue(tipTwo.text, tipIndex: 1)
+    }
+
+    @IBAction func thirdTipSlider(sender: AnyObject) {
+        var currentValue = sliderThree.value
+        tipThree.text = String(format: "%.2f", currentValue)
+        setTipValue(tipThree.text, tipIndex: 2)
+    }
+    
+    @IBAction func tipOneChanged(sender: AnyObject) {
+        sliderOne.value = (tipOne.text as NSString).floatValue
+        tipPercentages[0] = (tipOne.text as NSString).doubleValue
+        setTipValue(tipOne.text, tipIndex: 0)
+    }
+    
+    @IBAction func tipTwoChanged(sender: AnyObject) {
+        sliderTwo.value = (tipTwo.text as NSString).floatValue
+        setTipValue(tipTwo.text, tipIndex: 1)
+    }
+    
+    @IBAction func tipThreeChanged(sender: AnyObject) {
+        sliderThree.value = (tipThree.text as NSString).floatValue
+        setTipValue(tipThree.text, tipIndex: 2)
+    }
+    
+    func setTipValue(tipValue: String, tipIndex: Int){
+        tipPercentages[tipIndex] = (tipValue as NSString).doubleValue
+    }
+    
+    @IBAction func setTheme(sender: AnyObject) {
+        if(selectedTheme.selectedSegmentIndex == 0){
+            self.navigationController?.navigationBar.barTintColor = nil
+            UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.Default
+            self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.blackColor()]
+        } else if(selectedTheme.selectedSegmentIndex == 1){
+            self.navigationController?.navigationBar.barTintColor = UIColor.blackColor()
+            UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
+            self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
-
+var timeAtEnterBackground = NSDate().timeIntervalSince1970
+var tipPercentages:[Double] = [15, 20, 25]
 class ViewController: UIViewController {
-
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "willEnterForegroundNotification", name: UIApplicationWillEnterForegroundNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didEnterBackgroundNotification", name: UIApplicationDidEnterBackgroundNotification, object: nil)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        for i in 0...2 {
+            tipControl.setTitle(String(format: "%.2f%%", tipPercentages[i]), forSegmentAtIndex: i)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
 
+    @IBAction func onEditing(sender: AnyObject) {
+        var tipPercentage : Double = tipPercentages[tipControl.selectedSegmentIndex]
+        var userEnteredValue = (amountTextField.text as NSString).doubleValue
+        var tmpTipAmount = userEnteredValue*tipPercentage/100
+        var tmpTotalAmount = userEnteredValue+tmpTipAmount
+        tipAmount.text = currencyFormatter.stringFromNumber(tmpTipAmount)
+        totalAmount.text = currencyFormatter.stringFromNumber(tmpTotalAmount)
+    }
+    
+    var currencyFormatter: NSNumberFormatter {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        return formatter
+    }
+    
+    func willEnterForegroundNotification(){
+        var timeAtEnterForground = NSDate().timeIntervalSince1970
+        if ((timeAtEnterForground-timeAtEnterBackground)>600){
+            NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "amount")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            amountTextField.text = NSUserDefaults.standardUserDefaults().objectForKey("amount") as? String
+        }
+    }
+    
+    func didEnterBackgroundNotification(){
+        timeAtEnterBackground = NSDate().timeIntervalSince1970
+        NSUserDefaults.standardUserDefaults().setObject(amountTextField.text, forKey: "amount")
+        NSUserDefaults.standardUserDefaults().synchronize()
+        let amount: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey("amount")
+        println("\(amount)")
+    }
 }
 

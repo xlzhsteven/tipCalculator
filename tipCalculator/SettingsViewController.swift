d88a41ecbb3f6087aaef5071009cb63d01a77bd4
Xiaolong Zhang <xlzhsteven@gmail.com>
2015-01-16 21:36:13 -0800

    Clean up

--- tipCalculator/SettingsViewController.swift
+++ tipCalculator/SettingsViewController.swift
@@ -23,9 +23,12 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
         selectedTheme.selectedSegmentIndex = segmentedControlSelection
         setThemeBase(segmentedControlSelection)
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func opTap(sender: AnyObject) {
@@ -108,14 +111,4 @@ class SettingsViewController: UIViewController {
             }
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
